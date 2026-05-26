a73eaf65922eda326dc2b1f1a84266509211467f
Vishnu <vishnu@rigaps.com>
2026-05-26 08:04:16 +0530

    update: onClick function

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import { useState } from "react"
 function App() {
   const [color, setColor] = useState("olive")
 
+  const updateColor = (newColor) => {
+    setColor(newColor)
+  }
+
   return (
     <div className="w-full h-screen duration-200"
     style={{backgroundColor: color}}
@@ -11,7 +15,7 @@ function App() {
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
           <button
-          onClick={() => setColor("red")}
+          onClick={() => updateColor('red')}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "red"}}
           >Red</button>
@@ -21,7 +25,7 @@ function App() {
           style={{backgroundColor: "green"}}
           >Green</button>
           <button
-          onClick={() => setColor("blue")}
+          onClick={() => updateColor("blue")}
           className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
           style={{backgroundColor: "blue"}}
           >Blue</button>
